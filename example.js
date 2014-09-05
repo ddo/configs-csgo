dcb84f14a9a2b9eb00738516801e47d6779222c9
Ddo <joeddo89@gmail.com>
2014-09-05 17:59:42 +0700

    refactor

--- example.js
+++ example.js
@@ -2,5 +2,65 @@ var CSGO = require('./');
 
 var csgo = CSGO();
 
-// csgo.run();
-csgo.test();
+// csgo.test();
+// csgo.checkDir();
+
+// csgo.autobuy.open();
+
+// csgo.autobuy.get(function(err, weapons) {
+//     console.log(err);
+//     console.log(weapons);
+// });
+
+// var weapons = [
+//     'awp',
+//     'm4a1',
+//     'ak47',
+//     'p90',
+//     'primammo',
+//     'secammo',
+//     'vesthelm',
+//     'vest',
+//     'defuser',
+//     'hegrenade',
+//     'flashbang',
+//     'smokegrenade',
+//     'molotov',
+//     'incgrenade'
+// ];
+
+// csgo.autobuy.set(weapons, function(err){
+//     console.log(err);
+// });
+
+// csgo.autoexec.open();
+
+// csgo.autoexec.get(function(err, commands) {
+//     console.log(err);
+//     console.log(commands);
+// });
+
+// var commands = [
+//     'developer "1";',
+//     'con_filter_text "Damage";',
+//     'con_filter_text_out "Player:";',
+//     'con_filter_enable "2";'
+// ];
+// var commands = [];
+
+// csgo.autoexec.set(commands, function(err){
+//     console.log(err);
+// });
+
+// csgo.config.open();
+
+csgo.config.get(function(err, configs) {
+    console.log(err);
+    console.log(configs);
+});
+
+// var config = [];
+
+// csgo.config.set(config, function(err){
+//     console.log(err);
+// });
